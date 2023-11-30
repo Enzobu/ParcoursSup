2902d0edf465f72e595566e68fd724f88ba563cd
Enzobu <enzo.palermo@epsi.fr>
2023-11-30 13:44:59 +0100

    Set static roots

--- ParcoursStup/urls.py
+++ ParcoursStup/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.urls import path
 from .views import index
+from ParcoursSup import settings
 
 urlpatterns = [
-    path('', index),
-]
+    path('', index, name="index"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
