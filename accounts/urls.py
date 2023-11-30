d09deae165aa62eaccbbde3955ff5b537e7a6b93
Enzobu <enzo.palermo@epsi.fr>
2023-11-30 16:48:36 +0100

    add path for signup, login, logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from accounts.views import signup, logout_user, login_user
+
+urlpatterns = [
+    path('signup/', signup, name="signup"),
+    path('login/', login_user, name="login"),
+    path('logout/', logout_user, name="logout"),
+]
