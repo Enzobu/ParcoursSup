e5cd181b14aded0d58fa4279f7bdca45bd50073f
Enzobu <enzo.palermo@epsi.fr>
2023-12-09 09:26:50 +0100

    remove static to url

--- ParcoursStup/urls.py
+++ ParcoursStup/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls.static import static
 from django.urls import path
 from .views import index, school_detail
-from ParcoursSup import settings
 
 urlpatterns = [
     path('', index, name="index"),
     path('school/<str:slug>/', school_detail, name="school"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
