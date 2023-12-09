e064a2965d5708352156477543846c4db2ff95a8
Enzobu <enzo.palermo@epsi.fr>
2023-12-09 09:24:12 +0100

    add properties on user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUsers(AbstractUser):
-    pass
+    user_type = models.CharField(max_length=50)
+
+    ### Étudiant ###
+    birthday = models.DateField(default='2023-12-06', null=True)
+    report_card = models.FileField(upload_to="report-card", blank=True, null=True)
+    
+
+
+
+# cover_letter = models.TextField()
+# CV = models.FileField(upload_to="cv", blank=True, null=True)
