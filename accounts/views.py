6f873688d0960c5e94cd101a1af3808b9237ab7b
Enzobu <enzo.palermo@epsi.fr>
2023-12-09 09:25:30 +0100

    create view for signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,63 @@
 from django.contrib.auth import get_user_model, login, logout, authenticate
 from django.shortcuts import redirect, render
 
+from ParcoursStup.models import School
+
 User = get_user_model()
 
 def signup(request):
     if request.method == 'POST':
-        name = request.POST.get("name")
-        password = request.POST.get("password")
-        user = User.objects.create_user(username = name,
-                                        password = password)
+        user_type = request.POST.get("user_type")
+        if user_type == 'student':
+            username = request.POST.get("username")
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            email = request.POST.get("email")
+            password = request.POST.get("password")
+            birthday = request.POST.get("birthday")
+            report_card = request.POST.get("report_card")
+
+            user = User.objects.create_user(user_type = user_type,
+                                            username = username,
+                                            first_name = first_name,
+                                            last_name = last_name,
+                                            email = email,
+                                            password = password,
+                                            birthday = birthday,
+                                            report_card = report_card)
+
+        elif user_type == 'school':
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+            user = User.objects.create_user(user_type = user_type,
+                                            username = username,
+                                            password = password,)
+
+            name = request.POST.get("name")
+            description = request.POST.get("description")
+            slug = request.POST.get("name")
+            mail = request.POST.get("email")
+            address = request.POST.get("address")
+            city = request.POST.get("city")
+            region = request.POST.get("region")
+            department = request.POST.get("department")
+            zipCode = request.POST.get("zipCode")
+            picture = request.POST.get("picture")
+
+            school = School(name = name,
+                            description = description,
+                            slug = slug,
+                            mail = mail,
+                            address = address,
+                            city = city,
+                            region = region,
+                            department = department,
+                            zipCode = zipCode,
+                            picture = picture)
+            school.save()
+            print(username+', '+password+', '+name+', '+description+', '+slug+', '+mail+', '+address+', '+city+', '+region+', '+department+', '+zipCode+', '+picture)
+
         login(request, user)
         return redirect('index')
     
@@ -16,10 +65,11 @@ def signup(request):
 
 def login_user(request):
     if request.method == 'POST':
-        name = request.POST.get("name")
+        username = request.POST.get("username")
         password = request.POST.get("password")
-        user = authenticate(username = name, 
+        user = authenticate(username = username,
                             password = password)
+        print(f"email : {username}, password : {password}")
         if user:
             login(request, user)
             return redirect('index')
