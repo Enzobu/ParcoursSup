13fcfd873df6b2c702951119b587966d694ae2da
Enzobu <enzo.palermo@epsi.fr>
2023-12-13 11:15:39 +0100

    add school field

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from ParcoursStup.models import Formation, School
+
 class CustomUsers(AbstractUser):
     user_type = models.CharField(max_length=50)
 
     ### Étudiant ###
     birthday = models.DateField(default='2023-12-06', null=True)
     report_card = models.FileField(upload_to="report-card", blank=True, null=True)
-    
 
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='userSchool', null=True)
+
+class UserFormation(models.Model):
+    userStudent = models.ForeignKey(CustomUsers, on_delete=models.CASCADE, related_name='UserFormationUser')
+    formation = models.ForeignKey(Formation, on_delete=models.CASCADE, related_name='UserFormationFormation')
+    coverLettre = models.TextField()
 
 
 # cover_letter = models.TextField()
