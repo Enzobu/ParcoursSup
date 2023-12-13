82543fa416b1200662e35d42a1742e22eed8b0c3
Enzobu <enzo.palermo@epsi.fr>
2023-12-13 11:25:29 +0100

    add formation view

--- ParcoursStup/views.py
+++ ParcoursStup/views.py
@@ -29,3 +29,29 @@ def formation(request):
     return render(request, "ParcoursStup/formation.html", context={"formations": formations,
                                                                    "userFormations": userFormations})
 
+def add_formation(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        duration = request.POST.get("duration")
+        diploma = request.POST.get("diploma")
+        skills = request.POST.get("skills")
+        cost = request.POST.get("cost")
+        school = request.POST.get("school")
+
+        formation = Formation(
+            name = name,
+            description = description,
+            duration = duration,
+            diploma = diploma,
+            skills = skills,
+            cost = cost,
+            slug = name,
+            school = School.objects.get(name=school)
+        )
+        formation.save()
+        return redirect('formation')
+    
+    schools = School.objects.all()
+    return render(request, "ParcoursStup/add-formation.html", context={"schools": schools})
+
