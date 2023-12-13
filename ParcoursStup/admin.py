ca6d69537f68365ff84f7d69ca2aa7f244a9b5f8
Enzobu <enzo.palermo@epsi.fr>
2023-12-13 11:21:02 +0100

    add formation model to admin

--- ParcoursStup/admin.py
+++ ParcoursStup/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ParcoursStup.models import School
+from ParcoursStup.models import School, Formation
 
 # Register your models here.
 admin.site.register(School)
+admin.site.register(Formation)
