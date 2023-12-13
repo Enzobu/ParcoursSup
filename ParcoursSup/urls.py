3c81c105eec989d5f0e033eb6fa22f6dc045b9b4
Enzobu <enzo.palermo@epsi.fr>
2023-12-13 11:18:20 +0100

    update media_oot

--- ParcoursSup/urls.py
+++ ParcoursSup/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from ParcoursSup import settings
 
 urlpatterns = [
     path('', include("ParcoursStup.urls")),
     path('', include("accounts.urls")),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from accounts.views import signup, logout_user, login_user
-from django.conf.urls.static import static
-from ParcoursSup import settings
 
 urlpatterns = [
     path('signup/', signup, name="signup"),
     path('login/', login_user, name="login"),
     path('logout/', logout_user, name="logout"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
