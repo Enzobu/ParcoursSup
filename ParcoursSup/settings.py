611bd3534d4e93ae73fb260ec9624db7f54d326a
Enzobu <enzo.palermo@epsi.fr>
2023-12-13 11:23:12 +0100

    update media_root

--- ParcoursSup/settings.py
+++ ParcoursSup/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+AUTH_USER_MODEL = "accounts.CustomUsers"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
-AUTH_USER_MODEL = "accounts.CustomUsers"
