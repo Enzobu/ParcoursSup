dd8dbb8659e1182b889dd7116bc0637cc355798f
Enzobu <enzo.palermo@epsi.fr>
2023-12-13 11:14:54 +0100

    Update urls.py

--- ParcoursStup/urls.py
+++ ParcoursStup/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import index, school_detail
+from .views import index, school_detail, school_list, postuler, formation, add_formation, remove_formation
 
 urlpatterns = [
     path('', index, name="index"),
-    path('school/<str:slug>/', school_detail, name="school"),
+    path('school', school_list, name="school_list"),
+    path('school/<str:slug>/', school_detail, name="school_detail"),
+    path('school/<str:slug>/<str:formation>/', postuler, name="postuler"),
+    path('formation', formation, name="formation"),
+    path('add-formation', add_formation, name="add_formation"),
+    path('remove-formation/<str:slug>/', remove_formation, name="remove_formation"),
+    path('postuler/<str:slug>/', postuler, name="postuler"),
 ]
